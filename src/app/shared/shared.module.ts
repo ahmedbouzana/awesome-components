a76f51e283d2cf113a823da98fb39aea1040c254
ahmedbouzana <ah.bouzana@gmail.com>
2022-07-17 23:07:42 +0100

    Formatez vos données

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './components/comments/comments.component';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 
 
 @NgModule({
   declarations: [
-    CommentsComponent
+    CommentsComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule,
@@ -18,7 +20,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   exports: [
     CommentsComponent,
     MaterialModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule ,
+    ShortenPipe
   ]
 })
 export class SharedModule { }
