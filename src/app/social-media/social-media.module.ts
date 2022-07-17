10a819d835a1d2bee92469786bd9f6b57e3abf0a
AhmedBouzana <ah.bouzana@gmail.com>
2022-07-17 10:06:11 +0100

    Matérialisez les components

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -6,6 +6,7 @@ import { PostsService } from './services/posts.service';
 import { PostsResolver } from './resolvers/posts.resolver';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { PostListComponent } from './components/post-list/post-list.component';
     PostListComponent
   ],
   imports: [
-    CommonModule,
+    CommonModule,    
+    SharedModule,
     SocialMediaRoutingModule
   ],
   exports:[
