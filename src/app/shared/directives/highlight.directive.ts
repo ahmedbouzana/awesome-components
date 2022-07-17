91fb0309a0fa6450eeb07cf7e4d604e8161f7d7d
ahmedbouzana <ah.bouzana@gmail.com>
2022-07-17 23:53:08 +0100

    Soyez Directives

---
+++ src/app/shared/directives/highlight.directive.ts
@@ -0,0 +1,20 @@
+import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements AfterViewInit {
+
+  @Input() color = 'yellow';
+
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.setBackgroundColor(this.color);
+  }
+
+  setBackgroundColor(color: string) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+  }
+}
