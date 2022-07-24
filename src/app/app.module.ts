c6d0a1f48e10dad157c3dc6806c5abc8781e3e1c
ahmedbouzana <ah.bouzana@gmail.com>
2022-07-24 21:34:30 +0100

    Animez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     CoreModule,
     SharedModule
   ],
